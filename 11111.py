687889a734724ba878553ec83cea53af1baf4d13
maple224 <1056741859@qq.com>
2020-03-22 16:25:03 +0800

    第三次添加

--- 11111.py
+++ 11111.py
@@ -6,3 +6,18 @@ for i in range(scale):
     data[i]= list(map(int, input().split()))
 """
 #第二题初始化
+
+data = [[0]*scale for i in range(scale)]
+m, n = 0, 0
+
+for i in range(13):
+    list_temp = list(map(int, input().split()))
+    for j in range(len(list_temp)):
+        if(list_temp[j] != 0):
+            data[m][n] = list_temp[j]
+            data[n][m] = data[m][n]
+            m += 1
+        else:
+            m = 0
+            n += 1
+#第一题初始化
