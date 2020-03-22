f302aac983a698054db30d407baef078addd4495
maple224 <1056741859@qq.com>
2020-03-22 16:26:23 +0800

    第四次添加

--- 11111.py
+++ 11111.py
@@ -21,3 +21,25 @@ for i in range(13):
             m = 0
             n += 1
 #第一题初始化
+
+for i in range(scale):
+    for j in range(scale):
+        if data[i][j] == 0:
+            data[i][j] = 9999
+#数据更改,9999代表不连通
+            
+def swap(m, n, data):
+    data_swap = [[0]*scale for i in range(scale)]
+    
+    for i in range(scale):
+        for j in range(scale):
+            data_swap[i][j] = data[i][j]
+            if i == m:
+                data_swap[i][j] = data[n][j]
+            elif i == n:
+                data_swap[i][j] = data[m][j]
+            if j == m:
+                data_swap[i][j] = data[i][n]
+            elif j == n:
+                data_swap[i][j] = data[i][m]
+    return data_swap
